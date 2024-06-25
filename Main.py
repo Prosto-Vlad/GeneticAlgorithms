e2ed5a56d866ba1d38869924777c7a2229f5e766
Prosto-Vlad <vladdeynega8@gmail.com>
2024-06-25 20:04:07 +0300

    fix
    Прискорення

--- Main.py
+++ Main.py
@@ -22,66 +22,66 @@ if __name__ == '__main__':
         population = Population(chromosome_count[i], 10000, 1, purchases)
         population.fitness_all()
 
-        # i = 0
-        # stop_iter = 0
-        # max_fitness = 0
-        # timeList = []
-        # while stop_iter < 20:
-        #     classicAlgorithm = ClassicGenetic(population, 0.3, int(chromosome_count[i]*0.05))
-        #
-        #     start_time = time.time()
-        #
-        #     classicAlgorithm.genetic_algorithm()
-        #
-        #
-        #     end_time = time.time()
-        #
-        #     total_time_in_seconds = end_time - start_time
-        #     total_time_in_milliseconds = total_time_in_seconds * 1000
-        #     timeList.append(total_time_in_milliseconds)
-        #     stop_iter += 1
-        #
-        # print("Result")
-        # max_chrom = classicAlgorithm.get_max_fitness()
-        # max_chrom.print_test()
-        # print()
-        # mean_time = sum(timeList) / len(timeList)
-        # print(f"Classic genetic algorithm executed in {mean_time:.2f} milliseconds")
-        # for j in range(timeList.__len__()):
-        #     print(f"Time {j+1}: {timeList[j]:.2f} milliseconds")
-        #
-        # print()
+        i = 0
+        stop_iter = 0
+        max_fitness = 0
+        timeList = []
+        while stop_iter < 20:
+            classicAlgorithm = ClassicGenetic(population, 0.3, int(chromosome_count[i]*0.05))
 
-        for i in range(0, 20):
-            i = 0
-            stop_iter = 0
-            max_fitness = 0
-            timeList = []
-            population_tpm = Population(chromosome_count[i], 10000, 1, purchases)
-            population_tpm.fitness_all()
-            while stop_iter < 10:
-                population = population_tpm
-                parallelGenetic = ParallelGenetic(population, 0.3, int(2500 * 0.05))
+            start_time = time.time()
 
-                start_time = time.time()
+            classicAlgorithm.genetic_algorithm()
 
-                parallelGenetic.genetic_algorithm(6)
 
-                end_time = time.time()
+            end_time = time.time()
 
-                total_time_in_seconds = end_time - start_time
-                total_time_in_milliseconds = total_time_in_seconds * 1000
-                timeList.append(total_time_in_milliseconds)
-                stop_iter += 1
+            total_time_in_seconds = end_time - start_time
+            total_time_in_milliseconds = total_time_in_seconds * 1000
+            timeList.append(total_time_in_milliseconds)
+            stop_iter += 1
+
+        print("Result")
+        max_chrom = classicAlgorithm.get_max_fitness()
+        max_chrom.print_test()
+        print()
+        mean_time = sum(timeList) / len(timeList)
+        print(f"Classic genetic algorithm executed in {mean_time:.2f} milliseconds")
+        for j in range(timeList.__len__()):
+            print(f"Time {j+1}: {timeList[j]:.2f} milliseconds")
+
+        print()
+
+        #Розгін
+        # for i in range(0, 20):
+        #     i = 0
+        #     stop_iter = 0
+        #     max_fitness = 0
+        #     timeList = []
+        #     population_tpm = Population(chromosome_count[i], 10000, 1, purchases)
+        #     population_tpm.fitness_all()
+        #     while stop_iter < 10:
+        #         population = population_tpm
+        #         parallelGenetic = ParallelGenetic(population, 0.3, int(2500 * 0.05))
+        #
+        #         start_time = time.time()
+        #
+        #         parallelGenetic.genetic_algorithm(6)
+        #
+        #         end_time = time.time()
+        #
+        #         total_time_in_seconds = end_time - start_time
+        #         total_time_in_milliseconds = total_time_in_seconds * 1000
+        #         timeList.append(total_time_in_milliseconds)
+        #         stop_iter += 1
 
         i = 0
         stop_iter = 0
         max_fitness = 0
         timeList = []
-        population_tpm = Population(chromosome_count[i], 10000, 1, purchases)
-        population_tpm.fitness_all()
+        population = Population(chromosome_count[i], 10000, 1, purchases)
+        population.fitness_all()
         while stop_iter < 10:
-            population = population_tpm
             parallelGenetic = ParallelGenetic(population, 0.3, int(chromosome_count[i]*0.05))
 
             start_time = time.time()
